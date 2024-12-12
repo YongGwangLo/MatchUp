aef90dba680c3874fd4018a0f430633623fa4746
Daeseong Kim <daeseongkim05@gmail.com>
2024-12-12 16:10:56 +0900

    feat: Login 페이지 UI 완성

--- lib/constant/colors.dart
+++ lib/constant/colors.dart
@@ -7,4 +7,6 @@ class AppColors {
   static const Color lightGray = Color(0xFFD9D9D9);
   static const Color purple = Color(0xFF7F5CD6);
   static const Color onError = Color(0xFFFD7563);
+  static const Color naverGreen = Color(0xFF03C75A);
+  static const Color kakaoYellow = Color(0xFFFEE500);
 }
--- lib/ui/login/login_page.dart
+++ lib/ui/login/login_page.dart
@@ -1,10 +1,183 @@
 import 'package:flutter/material.dart';
+import 'package:match_up/constant/colors.dart';
 
-class LoginPage extends StatelessWidget {
+class LoginPage extends StatefulWidget {
   const LoginPage({super.key});
 
   @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+  late Animation<Color?> _gradientColor1;
+  late Animation<Color?> _gradientColor2;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _controller = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 3),
+    )..repeat(reverse: true);
+
+    _gradientColor1 = ColorTween(
+      begin: AppColors.black,
+      end: AppColors.purple,
+    ).animate(_controller);
+
+    _gradientColor2 = ColorTween(
+      begin: AppColors.purple,
+      end: AppColors.black,
+    ).animate(_controller);
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Text("LoginPage");
+    return Scaffold(
+      body: AnimatedBuilder(
+        animation: _controller,
+        builder: (context, child) {
+          return Container(
+            width: double.infinity,
+            height: double.infinity,
+            decoration: BoxDecoration(
+              gradient: LinearGradient(
+                colors: [_gradientColor1.value!, _gradientColor2.value!],
+                begin: Alignment.topLeft,
+                end: Alignment.bottomRight,
+              ),
+            ),
+            child: child,
+          );
+        },
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Stack(
+            children: [
+              // 중앙 텍스트
+              Center(
+                child: Transform.translate(
+                  offset: Offset(0, -50),
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      Text(
+                        "동네친구와 뜨거운 스포츠 한판!",
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 14,
+                            color: AppColors.white),
+                      ),
+                      SizedBox(
+                        height: 10,
+                      ),
+                      Image.asset(
+                        "assets/images/login_logo.png",
+                        width: 210,
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              Align(
+                alignment: Alignment.bottomCenter,
+                child: Padding(
+                  padding: const EdgeInsets.only(bottom: 20),
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                            textStyle: TextStyle(fontSize: 18),
+                            backgroundColor: AppColors.white,
+                            minimumSize: Size(double.infinity, 50),
+                            shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(10)),
+                            foregroundColor: AppColors.black),
+                        onPressed: () {},
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Image.asset(
+                              "assets/images/google_icon.png",
+                              width: 20,
+                              height: 20,
+                            ),
+                            Text("구글로 시작하기"),
+                            SizedBox(
+                              width: 24,
+                            ),
+                          ],
+                        ),
+                      ),
+                      SizedBox(height: 20),
+                      ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                            textStyle: TextStyle(fontSize: 18),
+                            backgroundColor: AppColors.naverGreen,
+                            minimumSize: Size(double.infinity, 50),
+                            shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(10)),
+                            foregroundColor: AppColors.white),
+                        onPressed: () {},
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Image.asset(
+                              "assets/images/naver_icon.png",
+                              width: 24,
+                              height: 24,
+                            ),
+                            Text("네이버로 시작하기"),
+                            SizedBox(
+                              width: 24,
+                            ),
+                          ],
+                        ),
+                      ),
+                      SizedBox(height: 20),
+                      ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                            textStyle: TextStyle(fontSize: 18),
+                            backgroundColor: AppColors.kakaoYellow,
+                            minimumSize: Size(double.infinity, 50),
+                            shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(10)),
+                            foregroundColor: AppColors.black),
+                        onPressed: () {},
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Image.asset(
+                              "assets/images/kakao_icon.png",
+                              width: 24,
+                              height: 24,
+                            ),
+                            Text("카카오로 시작하기"),
+                            SizedBox(
+                              width: 24,
+                            ),
+                          ],
+                        ),
+                      ),
+                      SizedBox(height: 20),
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
