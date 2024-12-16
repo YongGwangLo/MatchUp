798bd662d22180ebcae8f158c8a488e4e155899d
1125bid <dlfdlfdldh1125@naver.com>
2024-12-16 19:20:19 +0900

    refactor: import 정리

--- lib/ui/map/map_page.dart
+++ lib/ui/map/map_page.dart
@@ -2,15 +2,11 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_naver_map/flutter_naver_map.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:match_up/constant/colors.dart';
 import 'package:match_up/constant/categories.dart';
+import 'package:match_up/constant/colors.dart';
 import 'package:match_up/data/model/chat_rooms.dart';
-import 'package:match_up/data/repository/user_repository.dart';
-import 'package:match_up/ui/chat-submit/chat_submit_page.dart';
 import 'package:match_up/ui/map/map_page_view_model.dart';
 import 'package:match_up/ui/map/widgets/bottom_navigation_bar.dart';
-import 'package:match_up/ui/chat/chat_page.dart';
-import 'package:match_up/ui/mypage/mypage_page.dart';
 import 'package:match_up/ui/viewmodels/user_view_model.dart';
 
 class MapPage extends ConsumerStatefulWidget {
