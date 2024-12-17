35517bba49b1f39a1f8583441816389ffa7c728f
Daeseong Kim <daeseongkim05@gmail.com>
2024-12-18 02:33:12 +0900

    feat: mypage를 위해 bottomNavigation을 분리

---
+++ lib/ui/home/home_page.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import 'package:match_up/ui/map/map_page.dart';
+import 'package:match_up/ui/mypage/mypage_page.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  int _currentIndex = 0;
+
+  final List<Widget> _pages = [
+    const MapPage(),
+    const MypagePage(),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: _pages[_currentIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _currentIndex,
+        onTap: (index) {
+          setState(() {
+            _currentIndex = index;
+          });
+        },
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.map),
+            label: '지도 보기',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            label: '마이페이지',
+          ),
+        ],
+      ),
+    );
+  }
+}
